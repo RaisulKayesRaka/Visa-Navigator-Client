9665da928c9d772d1a4cace1cf99c2c7cadd7f3f
Raisul Kayes Raka <119594099+RaisulKayesRaka@users.noreply.github.com>
2024-12-07 22:59:44 +0600

    Add WhatWeOffer component to Home page

---
+++ src/components/WhatWeOffer.jsx
@@ -0,0 +1,59 @@
+import { FaTelegramPlane } from "react-icons/fa";
+
+export default function WhatWeOffer() {
+  return (
+    <>
+      <section className="py-8">
+        <section className="mx-auto w-11/12 max-w-screen-2xl">
+          <div className="text-center">
+            <div className="flex items-center justify-center gap-2 text-[#4682A9]">
+              <FaTelegramPlane />
+              <h3 className="font-semibold">What We Offer</h3>
+            </div>
+            <h2 className="text-2xl font-semibold">
+              Simplifying Your Journey with Tailored Visa Solutions
+            </h2>
+          </div>
+          <div className="h-8"></div>
+          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+            <div className="rounded bg-white p-6 border">
+              <h3 className="mb-4 text-xl font-semibold">
+                Comprehensive Visa Information
+              </h3>
+              <p>
+                Access detailed and accurate visa requirements for countries
+                around the globe, tailored to your travel needs.
+              </p>
+            </div>
+            <div className="rounded bg-white p-6 border">
+              <h3 className="mb-4 text-xl font-semibold">
+                Easy Application Process
+              </h3>
+              <p>
+                Seamlessly apply for visas online with our user-friendly forms
+                and guided steps.
+              </p>
+            </div>
+
+            <div className="rounded bg-white p-6 border">
+              <h3 className="mb-4 text-xl font-semibold">
+                Personalized Support
+              </h3>
+              <p>
+                Get assistance from our dedicated support team to address all
+                your visa-related queries.
+              </p>
+            </div>
+            <div className="rounded bg-white p-6 border">
+              <h3 className="mb-4 text-xl font-semibold">Fast and Reliable</h3>
+              <p>
+                Enjoy swift processing times and reliable updates, ensuring
+                you’re always on schedule.
+              </p>
+            </div>
+          </div>
+        </section>
+      </section>
+    </>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,14 @@
 import Banner from "../components/Banner";
 import LatestVisas from "../components/LatestVisas";
+import WhatWeOffer from "../components/WhatWeOffer";
 
 export default function Home() {
   return (
     <>
       <Banner />
       <LatestVisas />
+      <hr />
+      <WhatWeOffer />
     </>
   );
 }
