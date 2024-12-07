3e258a66ef2bbbeb94866aceab7ccc6623190041
Raisul Kayes Raka <119594099+RaisulKayesRaka@users.noreply.github.com>
2024-12-07 22:24:56 +0600

    Add LatestVisas component to Home page

---
+++ src/components/LatestVisas.jsx
@@ -0,0 +1,243 @@
+import { FaTelegramPlane } from "react-icons/fa";
+
+export default function LatestVisas() {
+  return (
+    <section className="py-8">
+      <div className="mx-auto w-11/12 max-w-screen-2xl">
+        <div className="text-center">
+          <div className="flex items-center justify-center gap-2 text-[#4682A9]">
+            <FaTelegramPlane />
+            <h3 className="font-semibold">Latest Visas</h3>
+          </div>
+          <h2 className="text-2xl font-semibold">
+            Explore the Newest Visa Opportunities Worldwide
+          </h2>
+        </div>
+        <div className="h-8"></div>
+        <section className="gird-cols-1 grid gap-4 sm:grid-cols-2 md:grid-cols-3">
+          <section className="rounded p-4 shadow">
+            <div className="h-48 rounded bg-[url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRofoHVTNeOXThr6qwXtFZjcCUJTgYcNG3cxA&s')] bg-cover bg-no-repeat text-white"></div>
+            <h3 className="p-2 text-center text-lg font-semibold text-[#4682A9]">
+              United State of America
+            </h3>
+            <hr />
+            <div className="my-2 flex justify-between">
+              <p className="font-semibold">Visa type</p>
+              <p>Tourist Visa</p>
+            </div>
+            <hr />
+            <div className="my-2 flex justify-between">
+              <p className="font-semibold">Procession time</p>
+              <p>5 business days</p>
+            </div>
+            <hr />
+            <div className="my-2 flex justify-between">
+              <p className="font-semibold">Fee</p>
+              <p>1800$</p>
+            </div>
+            <hr />
+            <div className="my-2 flex justify-between">
+              <p className="font-semibold">Validity</p>
+              <p>30 days</p>
+            </div>
+            <hr />
+            <div className="my-2 flex justify-between">
+              <p className="font-semibold">Application Mathod</p>
+              <p>Online</p>
+            </div>
+            <div className="mt-4">
+              <button className="w-full bg-[#4682A9] py-2 text-white">
+                See Details
+              </button>
+            </div>
+          </section>
+          <section className="rounded p-4 shadow">
+            <div className="h-48 rounded bg-[url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRofoHVTNeOXThr6qwXtFZjcCUJTgYcNG3cxA&s')] bg-cover bg-no-repeat text-white"></div>
+            <h3 className="p-2 text-center text-lg font-semibold text-[#4682A9]">
+              United State of America
+            </h3>
+            <hr />
+            <div className="my-2 flex justify-between">
+              <p className="font-semibold">Visa type</p>
+              <p>Tourist Visa</p>
+            </div>
+            <hr />
+            <div className="my-2 flex justify-between">
+              <p className="font-semibold">Procession time</p>
+              <p>5 business days</p>
+            </div>
+            <hr />
+            <div className="my-2 flex justify-between">
+              <p className="font-semibold">Fee</p>
+              <p>1800$</p>
+            </div>
+            <hr />
+            <div className="my-2 flex justify-between">
+              <p className="font-semibold">Validity</p>
+              <p>30 days</p>
+            </div>
+            <hr />
+            <div className="my-2 flex justify-between">
+              <p className="font-semibold">Application Mathod</p>
+              <p>Online</p>
+            </div>
+            <div className="mt-4">
+              <button className="w-full bg-[#4682A9] py-2 text-white">
+                See Details
+              </button>
+            </div>
+          </section>
+          <section className="rounded p-4 shadow">
+            <div className="h-48 rounded bg-[url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRofoHVTNeOXThr6qwXtFZjcCUJTgYcNG3cxA&s')] bg-cover bg-no-repeat text-white"></div>
+            <h3 className="p-2 text-center text-lg font-semibold text-[#4682A9]">
+              United State of America
+            </h3>
+            <hr />
+            <div className="my-2 flex justify-between">
+              <p className="font-semibold">Visa type</p>
+              <p>Tourist Visa</p>
+            </div>
+            <hr />
+            <div className="my-2 flex justify-between">
+              <p className="font-semibold">Procession time</p>
+              <p>5 business days</p>
+            </div>
+            <hr />
+            <div className="my-2 flex justify-between">
+              <p className="font-semibold">Fee</p>
+              <p>1800$</p>
+            </div>
+            <hr />
+            <div className="my-2 flex justify-between">
+              <p className="font-semibold">Validity</p>
+              <p>30 days</p>
+            </div>
+            <hr />
+            <div className="my-2 flex justify-between">
+              <p className="font-semibold">Application Mathod</p>
+              <p>Online</p>
+            </div>
+            <div className="mt-4">
+              <button className="w-full bg-[#4682A9] py-2 text-white">
+                See Details
+              </button>
+            </div>
+          </section>
+          <section className="rounded p-4 shadow">
+            <div className="h-48 rounded bg-[url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRofoHVTNeOXThr6qwXtFZjcCUJTgYcNG3cxA&s')] bg-cover bg-no-repeat text-white"></div>
+            <h3 className="p-2 text-center text-lg font-semibold text-[#4682A9]">
+              United State of America
+            </h3>
+            <hr />
+            <div className="my-2 flex justify-between">
+              <p className="font-semibold">Visa type</p>
+              <p>Tourist Visa</p>
+            </div>
+            <hr />
+            <div className="my-2 flex justify-between">
+              <p className="font-semibold">Procession time</p>
+              <p>5 business days</p>
+            </div>
+            <hr />
+            <div className="my-2 flex justify-between">
+              <p className="font-semibold">Fee</p>
+              <p>1800$</p>
+            </div>
+            <hr />
+            <div className="my-2 flex justify-between">
+              <p className="font-semibold">Validity</p>
+              <p>30 days</p>
+            </div>
+            <hr />
+            <div className="my-2 flex justify-between">
+              <p className="font-semibold">Application Mathod</p>
+              <p>Online</p>
+            </div>
+            <div className="mt-4">
+              <button className="w-full bg-[#4682A9] py-2 text-white">
+                See Details
+              </button>
+            </div>
+          </section>
+          <section className="rounded p-4 shadow">
+            <div className="h-48 rounded bg-[url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRofoHVTNeOXThr6qwXtFZjcCUJTgYcNG3cxA&s')] bg-cover bg-no-repeat text-white"></div>
+            <h3 className="p-2 text-center text-lg font-semibold text-[#4682A9]">
+              United State of America
+            </h3>
+            <hr />
+            <div className="my-2 flex justify-between">
+              <p className="font-semibold">Visa type</p>
+              <p>Tourist Visa</p>
+            </div>
+            <hr />
+            <div className="my-2 flex justify-between">
+              <p className="font-semibold">Procession time</p>
+              <p>5 business days</p>
+            </div>
+            <hr />
+            <div className="my-2 flex justify-between">
+              <p className="font-semibold">Fee</p>
+              <p>1800$</p>
+            </div>
+            <hr />
+            <div className="my-2 flex justify-between">
+              <p className="font-semibold">Validity</p>
+              <p>30 days</p>
+            </div>
+            <hr />
+            <div className="my-2 flex justify-between">
+              <p className="font-semibold">Application Mathod</p>
+              <p>Online</p>
+            </div>
+            <div className="mt-4">
+              <button className="w-full bg-[#4682A9] py-2 text-white">
+                See Details
+              </button>
+            </div>
+          </section>
+          <section className="rounded p-4 shadow">
+            <div className="h-48 rounded bg-[url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRofoHVTNeOXThr6qwXtFZjcCUJTgYcNG3cxA&s')] bg-cover bg-no-repeat text-white"></div>
+            <h3 className="p-2 text-center text-lg font-semibold text-[#4682A9]">
+              United State of America
+            </h3>
+            <hr />
+            <div className="my-2 flex justify-between">
+              <p className="font-semibold">Visa type</p>
+              <p>Tourist Visa</p>
+            </div>
+            <hr />
+            <div className="my-2 flex justify-between">
+              <p className="font-semibold">Procession time</p>
+              <p>5 business days</p>
+            </div>
+            <hr />
+            <div className="my-2 flex justify-between">
+              <p className="font-semibold">Fee</p>
+              <p>1800$</p>
+            </div>
+            <hr />
+            <div className="my-2 flex justify-between">
+              <p className="font-semibold">Validity</p>
+              <p>30 days</p>
+            </div>
+            <hr />
+            <div className="my-2 flex justify-between">
+              <p className="font-semibold">Application Mathod</p>
+              <p>Online</p>
+            </div>
+            <div className="mt-4">
+              <button className="w-full bg-[#4682A9] py-2 text-white">
+                See Details
+              </button>
+            </div>
+          </section>
+        </section>
+        <div className="h-8"></div>
+
+        <button className="w-full rounded border border-[#4682A9] p-1 font-semibold text-[#4682A9]">
+          See all visas
+        </button>
+      </div>
+    </section>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Banner from "../components/Banner";
+import LatestVisas from "../components/LatestVisas";
 
 export default function Home() {
   return (
     <>
       <Banner />
+      <LatestVisas />
     </>
   );
 }
