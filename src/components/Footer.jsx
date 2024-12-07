2422123460e08bbc65a269155ef5fc0c3d92f18c
Raisul Kayes Raka <119594099+RaisulKayesRaka@users.noreply.github.com>
2024-12-07 20:26:44 +0600

    Add Footer Component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,49 @@
+import { FaFacebook, FaLinkedin, FaTwitter } from "react-icons/fa";
+import { FaLocationDot } from "react-icons/fa6";
+import { IoIosCall } from "react-icons/io";
+import { IoMail } from "react-icons/io5";
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className="bg-[#F6F4EB]">
+      <div className="mx-auto w-11/12 max-w-screen-2xl">
+        <div className="flex flex-col sm:flex-row gap-4 items-center justify-between py-4">
+          <div className="flex items-center justify-center gap-2">
+            <img
+              src="/visa-navigator-logo-48.png"
+              className="h-8"
+              alt="Visa Navigator Logo"
+            />
+            <span className="self-center whitespace-nowrap text-2xl font-semibold">
+              Visa Navigator
+            </span>
+          </div>
+          <div className="flex flex-col lg:flex-row justify-center items-center lg:gap-8">
+            <div className="flex items-center gap-2">
+              <FaLocationDot />
+              <p>Dhaka, Bangladesh</p>
+            </div>
+            <div className="flex items-center gap-2">
+              <IoIosCall />
+              <p>+880123456789</p>
+            </div>
+            <div className="flex items-center gap-2">
+              <IoMail />
+              <p>info@visanavigator.com</p>
+            </div>
+          </div>
+        </div>
+        <hr />
+        <div className="flex flex-col-reverse items-center justify-between gap-4 py-4 sm:flex-row">
+          <div>
+            <p>&copy; Copyright 2024. All rights reserved.</p>
+          </div>
+          <div className="flex gap-4">
+            <FaTwitter />
+            <FaFacebook />
+            <FaLinkedin />
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
